20c61ca5509c863b2695ba1987533a7aa77ef73c
Nikolnikoff <mikael.nikolnikov@nordsec.com>
2023-09-29 10:57:32 +0300

    chore: create buttons and logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,50 @@
+import { useState } from 'react'
 import './App.css'
 
-function App() {
+enum Forms {
+  Login = 'Login',
+  Register = 'Register',
+  ForgotPassword = 'Forgot Password',
+  ChangePassword = 'Change Password',
+  PersonalInfo = 'Personal Info',
+  CreditCard = 'Credit Card',
+}
 
+function App() {
+  const [visibleForm, setVisibleForm] = useState<Forms | string>('');
   return (
-   <div>Hello world!</div>
+    <div className="autofill-controls">
+      <div className="buttons">
+        <button onClick={() => setVisibleForm(Forms.Login)}>Login</button>
+        <button>Register</button>
+        <button>Forgot Password</button>
+        <button>Change Password</button>
+        <button>Personal Info</button>
+        <button>Credit Card</button>
+      </div>
+      <div className="forms">
+        <div className="formSample">
+          {visibleForm == Forms.Login && <form>
+            <label>
+              <input type="text" placeholder='Email Adress/Username' />
+            </label>
+            <label>
+              <input type="password" placeholder='Password' autoComplete='current-password' />
+            </label>
+            <button type="submit">Sign In</button>
+          </form>}
+          {visibleForm == Forms.Register && <form>
+            <label>
+              <input type="text" placeholder='Email Adress/Username' />
+            </label>
+            <label>
+              <input type="password" placeholder='Password' autoComplete='current-password' />
+            </label>
+            <button type="submit">Sign In</button>
+          </form>}
+        </div>
+      </div>
+    </div>
   )
 }
 
