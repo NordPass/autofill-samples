f9db700e0465e52a124ddccb0d9d5523604fa24a
Nikolnikoff <mikael.nikolnikov@nordsec.com>
2024-03-14 13:51:26 +0200

    refactor: mobile view

--- src/mobileView/MobileNavigation.tsx
+++ src/mobileView/MobileNavigation.tsx
@@ -3,17 +3,15 @@ import { Link } from "react-router-dom";
 
 export const MobileNavigation = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
-  if (toggleMenu) {
-    document.body.style.overflow = 'hidden';
-  } else document.body.style.overflow = 'auto';
+  toggleMenu ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'auto';
 
   return (
-    <nav className={`${toggleMenu ? 'block fixed h-full flex-wrap bg-base-100 overflow-auto' : ''}`}>
+    <nav className={`fixed ${toggleMenu && 'block fixed h-full flex-wrap bg-base-100 overflow-auto'}`}>
       <button
         className="btn btn-square btn-ghost transition-transform duration-300 ease-in-out"
         onClick={() => setToggleMenu(!toggleMenu)}>
-        {!toggleMenu && <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>}
-        {toggleMenu && <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg>}
+        {!toggleMenu ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg> :
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg>}
       </button>
 
       {toggleMenu && <div
