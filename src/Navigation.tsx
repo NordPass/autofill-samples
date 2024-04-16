06de68173c0ed7218d3cf9c420fb32398f82b326
Nikolnikoff <mikael.nikolnikov@nordsec.com>
2024-04-16 10:07:45 +0300

    chore: add second login form

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -4,6 +4,7 @@ export const Navigation = () => (
   <nav className="flex justify-around m-3 flex-wrap">
     <Link className="btn ml-2" to="/all">All Forms</Link>
     <Link className="btn ml-2" to="/login">Login</Link>
+    <Link className="btn ml-2" to="/loginPhone">Login 2</Link>
     <Link className="btn ml-2" to="/register">Register</Link>
     <Link className="btn ml-2" to="/forgotPassword">Forgot Password</Link>
     <Link className="btn ml-2" to="/changePassword">Change Password</Link>
