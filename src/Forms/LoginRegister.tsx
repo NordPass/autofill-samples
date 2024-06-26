82ed2a14b87ae5dde417706ca5ff7a0ffa6caddd
Nikolnikoff <mikael.nikolnikov@nordsec.com>
2024-06-26 11:55:25 +0300

    fix: login_register form

--- src/Forms/LoginRegister.tsx
+++ src/Forms/LoginRegister.tsx
@@ -34,7 +34,7 @@ export const LoginRegister = () => {
         Clear
       </button>
       <form className="flex flex-col items-start mt-3">
-        <h1 className="text-4xl mb-3">Enter email to sign in or sign up!</h1>
+        <h1 className="text-4xl mb-3">Log in or sign up</h1>
         <input
           ref={email}
           className="input input-bordered w-full max-w-xs"
