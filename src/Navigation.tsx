c8ca71c12d96539ee3a314b7107b4c3a6758ffee
Nikolnikoff <mikael.nikolnikov@nordsec.com>
2024-08-27 09:40:44 +0300

    refactor: nav bar

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,31 +1,31 @@
 import { Link } from 'react-router-dom';
 
 export const Navigation = () => (
-  <nav className="flex justify-around m-3 flex-wrap">
-    <Link className="btn ml-2" to="/all">All Forms</Link>
-    <Link className="btn ml-2" to="/login">Login</Link>
-    <Link className="btn ml-2" to="/loginPhone">Login 2</Link>
-    <Link className="btn ml-2" to="/register">Register</Link>
-    <Link className="btn ml-2" to="/forgotPassword">Forgot Password</Link>
-    <Link className="btn ml-2" to="/changePassword">Change Password</Link>
-    <Link className="btn ml-2" to="/changeEmail">Change Email</Link>
-    <Link className="btn ml-2" to="/changeEmailWithPassword">Change Email 2</Link>
-    <Link className="btn ml-2" to="/creditCard">Credit Card</Link>
-    <Link className="btn ml-2" to="/identity">Personal Info</Link>
-    <Link className="btn ml-2" to="/identitySelects">Personal Info 2</Link>
-    <Link className="btn ml-2" to="/identityPhone">Personal Info 3</Link>
-    <Link className="btn ml-2" to="/other">Other</Link>
-    <Link className="btn ml-2" to="/subscribe">Subscribe</Link>
-    <Link className="btn ml-2" to="/mfa">One Time Password</Link>
-    <Link className="btn ml-2" to="/totp">TOTP</Link>
-    <Link className="btn ml-2" to="/loginRegister">Login/Register</Link>
-    <Link className="btn ml-2" to="/builder">Builder</Link>
-    <Link className="btn ml-2" to="/customLogin">Custom Login</Link>
-    <Link className="btn ml-2" to="/customLoginTwoFields">Custom Login 2</Link>
-    <Link className="btn ml-2" to="/secondStepLogin">Second Step Login</Link>
-    <Link className="btn ml-2" to="/hiddenSecondStepLogin">Second Step Login 2</Link>
-    <Link className="btn ml-2" to="/twoLogin">Two Login</Link>
-    <Link className="btn ml-2" to="/modalLogin">Modal Login</Link>
-    <Link className="btn ml-2" to="/checkoutForm">Checkout Form</Link>
+  <nav className="flex justify-around m-3 flex-wrap gap-2">
+    <Link className="btn" to="/all">All Forms</Link>
+    <Link className="btn" to="/login">Login</Link>
+    <Link className="btn" to="/loginPhone">Login 2</Link>
+    <Link className="btn" to="/register">Register</Link>
+    <Link className="btn" to="/forgotPassword">Forgot Password</Link>
+    <Link className="btn" to="/changePassword">Change Password</Link>
+    <Link className="btn" to="/changeEmail">Change Email</Link>
+    <Link className="btn" to="/changeEmailWithPassword">Change Email 2</Link>
+    <Link className="btn" to="/creditCard">Credit Card</Link>
+    <Link className="btn" to="/identity">Personal Info</Link>
+    <Link className="btn" to="/identitySelects">Personal Info 2</Link>
+    <Link className="btn" to="/identityPhone">Personal Info 3</Link>
+    <Link className="btn" to="/other">Other</Link>
+    <Link className="btn" to="/subscribe">Subscribe</Link>
+    <Link className="btn" to="/mfa">One Time Password</Link>
+    <Link className="btn" to="/totp">TOTP</Link>
+    <Link className="btn" to="/loginRegister">Login/Register</Link>
+    <Link className="btn" to="/builder">Builder</Link>
+    <Link className="btn" to="/customLogin">Custom Login</Link>
+    <Link className="btn" to="/customLoginTwoFields">Custom Login 2</Link>
+    <Link className="btn" to="/secondStepLogin">Second Step Login</Link>
+    <Link className="btn" to="/hiddenSecondStepLogin">Second Step Login 2</Link>
+    <Link className="btn" to="/twoLogin">Two Login</Link>
+    <Link className="btn" to="/modalLogin">Modal Login</Link>
+    <Link className="btn" to="/checkoutForm">Checkout Form</Link>
   </nav>
 );
