fce262ecde6ba0767c006f2c4c5c55d5120607d8
Nikolnikoff <mikael.nikolnikov@nordsec.com>
2024-09-05 14:41:20 +0300

    fix: totp forms

--- src/Forms/TOTPOneField.tsx
+++ src/Forms/TOTPOneField.tsx
@@ -1,7 +1,7 @@
 export const TOTPOneField = () => (
   <form className="flex items-center justify-center mt-3 flex-col">
     <h1 className="text-4xl mb-3 mt-3">Enter your code</h1>
-    <p className="mb-3">Enter the 6-digit security code from your authenticator app.</p>
+    <p className="mb-3">Enter the 6-digit code from your authenticator app.</p>
 
     <div className="flex justify-around w-[300px]">
       <input className="input input-bordered"/>
