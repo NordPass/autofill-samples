c05315a5e17b435ad8d3a8e57e020f8cec771a40
Nikolnikoff <mikael.nikolnikov@nordsec.com>
2024-09-11 15:18:03 +0300

    refactor: totp forms

--- src/Forms/TOTP.tsx
+++ src/Forms/TOTP.tsx
@@ -1,17 +1,16 @@
 export const TOTP = () => (
   <form className="flex items-center justify-center mt-3 flex-col">
-    <h1 className="text-4xl mb-3 mt-3">Enter your code</h1>
-    <p className="mb-3">Enter the 6-digit code from your authenticator app.</p>
-
-    <div className="flex justify-around w-[300px]">
-      <input className="input input-bordered w-[40px]" maxLength={1} />
-      <input className="input input-bordered w-[40px]" maxLength={1} />
-      <input className="input input-bordered w-[40px]" maxLength={1} />
-      <input className="input input-bordered w-[40px]" maxLength={1} />
-      <input className="input input-bordered w-[40px]" maxLength={1} />
-      <input className="input input-bordered w-[40px]" maxLength={1} />
-    </div>
-
+    <fieldset className="TotpInput__loginFieldset flex items-center justify-center mt-3 flex-col">
+      <label className="TotpInput__loginTextFieldLabel text-4xl mb-3 mt-3"> Enter code from your mobile authenticator app: </label>
+      <div className="TotpInput__totpInputContainer flex justify-around w-[300px]">
+        <input type="text" autoComplete="off" maxLength={1} className="TotpInput__loginInput keeper-ignore input input-bordered w-[40px]" />
+        <input type="text" autoComplete="off" maxLength={1} className="TotpInput__loginInput keeper-ignore input input-bordered w-[40px]" />
+        <input type="text" autoComplete="off" maxLength={1} className="TotpInput__loginInput keeper-ignore input input-bordered w-[40px]" />
+        <input type="text" autoComplete="off" maxLength={1} className="TotpInput__loginInput keeper-ignore input input-bordered w-[40px]" />
+        <input type="text" autoComplete="off" maxLength={1} className="TotpInput__loginInput keeper-ignore input input-bordered w-[40px]" />
+        <input type="text" autoComplete="off" maxLength={1} className="TotpInput__loginInput keeper-ignore input input-bordered w-[40px]" />
+      </div>
+    </fieldset>
     <button className="btn btn-outline mt-3 w-[15%]" type="button">Submit</button>
   </form>
 );
--- src/Forms/TOTPOneField.tsx
+++ src/Forms/TOTPOneField.tsx
@@ -1,12 +1,18 @@
 export const TOTPOneField = () => (
-  <form className="flex items-center justify-center mt-3 flex-col">
-    <h1 className="text-4xl mb-3 mt-3">Enter your code</h1>
-    <p className="mb-3">Enter the 6-digit code from your authenticator app.</p>
-
-    <div className="flex justify-around w-[300px]">
-      <input className="input input-bordered"/>
+  <form className="gl-show-field-errors js-2fa-form flex items-center justify-center mt-3 flex-col" aria-live="assertive" action="/users/sign_in" accept-charset="UTF-8" method="post">
+    <input type="hidden" name="authenticity_token" value="b7NGhc4zZ4o6OsmTX7e_tdLsFoI92Vrqbv410JjU2miRg-bVxyyetxKk7zG6ij-V3528_j6wfJ681mU-U95VQQ" autoComplete="off" />
+    <div className="form-group flex justify-around w-[500px] flex-col">
+      <label htmlFor="user_otp_attempt" className="text-4xl mb-2">Enter verification code</label>
+      <input className="form-control gl-form-input input input-bordered" title="This field is required." data-testid="two-fa-code-field" type="text" name="user[otp_attempt]" id="user_otp_attempt" />
+      <p className="gl-field-error hidden">This field is required.</p>
+      <p className="form-text text-muted hint mt-4">
+        Enter the code from your two-factor authenticator app.
+        If you've lost your device, you can enter one of your recovery codes.
+      </p>
+      <input value="0" autoComplete="off" type="hidden" name="user[remember_me]" id="user_remember_me" />
     </div>
-
-    <button className="btn btn-outline mt-3 w-[7%]" type="button">Submit</button>
+    <button data-testid="verify-code-button" type="submit" className="btn btn-outline mt-3 w-[7%]">
+      <span className="gl-button-text"> Verify code </span>
+    </button>
   </form>
 );
