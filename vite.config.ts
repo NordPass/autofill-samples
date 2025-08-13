3d30f6d74927078c73c4982ec27ea31e71462e6b
Justinas Reigis <justinas.reigis@nordsec.com>
2025-08-13 16:33:05 +0300

    chore: dependencies with react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
+    tailwindcss(),
+  ],
   base: '/autofill-samples/',
 });
